6df9d1a1575254b01c8caaf3422ec5e505e4c439
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-04 11:48:19 -0300

    chore: Otimiza os imports

--- server/src/main/java/tech/vault/server/ServerApplication.java
+++ server/src/main/java/tech/vault/server/ServerApplication.java
@@ -1,10 +1,7 @@
 package tech.vault.server;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ServerApplication {
--- server/src/main/java/tech/vault/server/core/controller/ComputerController.java
+++ server/src/main/java/tech/vault/server/core/controller/ComputerController.java
@@ -3,16 +3,16 @@ package tech.vault.server.core.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tech.vault.server.core.dto.ComputerRequestBuilder;
 import tech.vault.server.core.dto.ComputerResponseBuilder;
 import tech.vault.server.core.service.ComputerService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/computer")
+@RequestMapping("v1/computer")
 public class ComputerController {
     @Autowired
     private ComputerService service;
@@ -20,5 +20,31 @@ public class ComputerController {
     @GetMapping()
     public ResponseEntity<List<ComputerResponseBuilder>> getAllComputers() {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAllComputers());
+    }
+
+    @GetMapping("/{computer-id}")
+    public ResponseEntity<ComputerResponseBuilder> getComputerById(@PathVariable("computer-id") UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getComputerById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postComputer(@RequestBody ComputerRequestBuilder request) {
+        service.setComputer(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Computador inserido com sucesso!");
+    }
+
+    @PatchMapping("/{computer-id}")
+    public ResponseEntity<String> patchComputer(@PathVariable("computer=id") UUID id, @RequestBody ComputerRequestBuilder request) {
+        service.patchComputer(id, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Alteração realizada com sucesso!");
+    }
+
+    @DeleteMapping("/{computer-id}")
+    public ResponseEntity<String> deleteComputer(@PathVariable("computer=id") UUID id) {
+        service.deleteComputer(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Computador deletado com sucesso!");
     }
 }
