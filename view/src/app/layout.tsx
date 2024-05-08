c74df421973e9ae71b16d6f52ba29a6e7a91c8e2
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-08 02:48:51 -0300

    feat: Introduz o novo front-end
    - Front-end segue com o uso de next.js
    - Material-ui e tailwind
    - Axios

---
+++ view/src/app/layout.tsx
@@ -0,0 +1,31 @@
+// Material-ui
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../components/themes/theme";
+// Next.js
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Criado com next.js",
+  description: "Vinícius Gabriel - Criado com next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
