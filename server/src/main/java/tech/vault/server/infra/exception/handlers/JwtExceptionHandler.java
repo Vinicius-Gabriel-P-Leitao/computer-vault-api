d3c94dc05635d3b418f83f347576a6c6a7a8fcf8
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-11 02:02:31 -0300

    feat: Insere uma tratamento de erros caso o JWT está deformado

---
+++ server/src/main/java/tech/vault/server/infra/exception/handlers/JwtExceptionHandler.java
@@ -0,0 +1,20 @@
+package tech.vault.server.infra.exception.handlers;
+
+import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import tech.vault.server.infra.exception.message.OperationStatus;
+
+@ControllerAdvice
+public class JwtExceptionHandler {
+    private OperationStatus operationStatus;
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<OperationStatus> handleMalformedJwtException(MalformedJwtException ex, WebRequest request) {
+        operationStatus = new OperationStatus(HttpStatus.BAD_REQUEST, "Token JWT inválido: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(operationStatus);
+    }
+}
--- server/src/main/java/tech/vault/server/infra/exception/handlers/RestExceptionHandler.java
+++ server/src/main/java/tech/vault/server/infra/exception/handlers/RestExceptionHandler.java
@@ -1,11 +1,9 @@
 package tech.vault.server.infra.exception.handlers;
 
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
