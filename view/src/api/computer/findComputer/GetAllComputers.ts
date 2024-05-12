a027cfe935d4ebf02d1eed3f5c953f54c1f23188
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-12 17:08:56 -0300

    feat: Inicia o uso das rotas de crud dos computadores
    - Foi feito somente a de getAll

---
+++ view/src/api/computer/findComputer/GetAllComputers.ts
@@ -0,0 +1,34 @@
+"use server";
+import { cookies } from "next/headers";
+
+async function GetAllComputers(url: string) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    throw new Error("Token not found in cookies");
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token.value}`,
+      },
+    });
+
+    if (!response.ok) {
+      return { operation: false, message: "Erro ao realizar requisição" };
+    }
+
+    const jsonResponse = await response.json();
+
+    return { status: true, result: jsonResponse };
+  } catch (error) {
+    return { operation: false, message: "Erro interno ao realizar requisição" };
+  }
+}
+
+export default GetAllComputers;
