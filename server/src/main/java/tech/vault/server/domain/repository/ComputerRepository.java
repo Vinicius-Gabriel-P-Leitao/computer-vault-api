4602f32904f213c1811199cdbf7f6f041fef23d5
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-26 16:39:20 -0300

    feat: Adiciona novos métodos de count no repositório de computador
    - Count memory ram que irá buscar a quantidade de diversidade que tem relacionado a memoria ram no banco de dados

--- server/src/main/java/tech/vault/server/domain/repository/ComputerRepository.java
+++ server/src/main/java/tech/vault/server/domain/repository/ComputerRepository.java
@@ -1,17 +1,20 @@
 package tech.vault.server.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import tech.vault.server.domain.entity.Computer;
-import tech.vault.server.domain.entity.values.*;
 
 import java.util.List;
 
 @Repository
 public interface ComputerRepository extends JpaRepository<Computer, Integer> {
-    @Query("SELECT c.locationComputer, COUNT(c) FROM Computer c GROUP BY c.locationComputer")
+    @Query("select tb.locationComputer valores, count(tb) quantidade from Computer tb group by tb.locationComputer")
     List<Object[]> countComputersByLocation();
+
+    @Query("select tb.memoryRam valores, count(tb.memoryRam) quantidade from Computer tb group by tb.memoryRam")
+    List<Object[]> countMemoryRam();
+
+    @Query("select count(*) total_registros from Computer tb")
+    Integer countAllComputers();
 }
