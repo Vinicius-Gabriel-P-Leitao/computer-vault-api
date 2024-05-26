f22f72bef81a7758d9b388b2641b336208101c85
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-05-26 16:42:45 -0300

    feat: Controllers para novas rotas de count

---
+++ server/src/main/java/tech/vault/server/core/service/implement/ComputerCountMemoryRamServiceImpl.java
@@ -0,0 +1,32 @@
+package tech.vault.server.core.service.implement;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.vault.server.core.service.ComputerCountMemoryRamService;
+import tech.vault.server.domain.repository.ComputerRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class ComputerCountMemoryRamServiceImpl implements ComputerCountMemoryRamService {
+    @Autowired
+    ComputerRepository repository;
+
+    @Override
+    public Map<Integer, Long> countComputerByMemoryRam() {
+        List<Object[]> result = repository.countMemoryRam();
+
+        Map<Integer, Long> memoryRamCountMap = new HashMap<>();
+        for (Object[] response : result) {
+            Integer location = (Integer) response[0];
+            Long count = (Long) response[1];
+            memoryRamCountMap.put(location, count);
+        }
+
+        return memoryRamCountMap;
+    }
+}
